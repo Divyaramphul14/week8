d0c60e31631350d6e4c6f115b1fdf90180486f95
divyaramphul <20807760@student.curtin.edu.au>
2023-09-21 11:09:33 +0400

    calculations

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+# BAD
+def add(a, b):
+    return float(a + b)
+
+def subtract(a, b):
+    return float(a - b)
+
+def multiply(a, b):
+    return float(a * b)
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("division by zero")
+    return float(a / b)
